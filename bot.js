0c31a7439646aad072fa91a51c7c799e4c28381f
Forcellrus <forcellrus@hotmail.com>
2022-03-06 17:16:58 +0100

    i fix

--- bot.js
+++ bot.js
@@ -1,17 +1,37 @@
 require('dotenv').config();
 
-
-const { Client, Intents } = require('discord.js');
+const { Client, Intents: { FLAGS } } = require('discord.js');
 const bot = new Client({
-	intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS, Intents.GUILD_MEMBERS],
+	intents: [
+		FLAGS.GUILDS,
+		FLAGS.GUILD_MESSAGES,
+	],
 });
-
 bot.login(process.env.BOT_TOKEN);
 
-bot.on('messageCreate', (message => {
-	if (message.channel.id !== process.env.CHANNEL_ID) return;
-	if (!message.content.includes('https://twitter.com/')) return;
-	if (!message.embeds[0].video) return;
-	message.channel.send(message.content.replace('twitter','fxtwitter'));
+bot.on('ready', (() => {
+	console.log('Ready!');
+}));
+
+const isTwitterVideo = (message) => {
+	return (
+		message.channel.id === process.env.CHANNEL_ID &&
+		message.content.includes('https://twitter.com/') &&
+		message.embeds[0].video !== undefined
+	);
+};
+
+const postFixedLink = (message) => {
+	const retardWarning = `<@${message.author.id}> post fxtwitter link retard`;
+	const fixedLink = message.content.replace('https://twitter.com/', 'https://fxtwitter.com/');
+	message.channel.send(`${retardWarning}\n${fixedLink}`);
 	message.delete();
+};
+
+bot.on('messageCreate', ((message) => {
+	if (isTwitterVideo(message)) postFixedLink(message);
+}));
+
+bot.on('messageUpdate', ((_old, message) => {
+	if (isTwitterVideo(message)) postFixedLink(message);
 }));
